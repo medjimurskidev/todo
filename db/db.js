46dc29b414583e725b1b0bd7e1502f89272c984d
Roberto Kedmenec <rexerd96@gmail.com>
2019-03-05 16:35:33 +0100

    Init commit.

---
+++ db/db.js
@@ -0,0 +1,19 @@
+const todos = [
+    {
+        id: 1, 
+        title: 'Lunch',
+        description: 'Go for a lunch break at 2pm'
+    },
+    {
+        id: 2, 
+        title: 'Dinner',
+        description: 'Go for a dinner at 7pm'
+    },
+    {
+        id: 3, 
+        title: 'Sleep',
+        description: 'Go to sleep at 10pm'
+    }
+]
+
+export default todos; 
